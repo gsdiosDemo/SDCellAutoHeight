0b401c3a2c7d0d52ad37f922d4b93c0a0e701309
GSD_iOS <gsdios@126.com>
2016-01-03 17:25:35 +0800

    初始化demo

---
+++ SDAutoCellHeightDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDAutoCellHeightDemo
+//
+//  Created by aier on 16/1/3.
+//  Copyright © 2016年 GSD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDAutoCellHeightDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDAutoCellHeightDemo
+//
+//  Created by aier on 16/1/3.
+//  Copyright © 2016年 GSD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UITableViewController
+
+
+@end
+
